7011910f344d1f61f8f44b80e10b067287301231
bhagya554 <kudupudi.bhagya@gmail.com>
2025-12-30 17:17:11 +0530

    Remove print statement for 'Branch 1'

--- src/test/java/com/seleniumframeworkusingai/tests/api/Sample.java
+++ src/test/java/com/seleniumframeworkusingai/tests/api/Sample.java
@@ -3,6 +3,6 @@ package com.seleniumframeworkusingai.tests.api;
 public class Sample {
     public static void main(String[] args) {
         System.out.println("Jelllo");
-        System.out.println("Branch 1");
+        
     }
 }
