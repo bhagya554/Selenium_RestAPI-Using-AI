6b248f3cbfeac284c932cdab1fdc6f7d066dcbfd
Bhagya54 <testingtrainer54@gmail.com>
2025-12-30 17:21:21 +0530

    push to master

--- src/test/java/com/seleniumframeworkusingai/tests/api/Sample.java
+++ src/test/java/com/seleniumframeworkusingai/tests/api/Sample.java
@@ -3,6 +3,7 @@ package com.seleniumframeworkusingai.tests.api;
 public class Sample {
     public static void main(String[] args) {
         System.out.println("Jelllo");
+        System.out.println("Hello");
         
     }
 }
